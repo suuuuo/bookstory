3bd423ff45263ae1d300d76deaf0816a8933e5a0
ji-jjang <jinyhehe@gmail.com>
2024-04-03 03:43:10 +0900

    feat: 로그인, 로그아웃 토큰(access, refresh) 관리 및 필요한 Security Filter 설정

---
+++ src/main/java/com/elice/bookstore/config/security/authentication/jwt/JwtUtil.java
@@ -0,0 +1,107 @@
+package com.elice.bookstore.config.security.authentication.jwt;
+
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * JWT Utility.
+ */
+@Component
+@Slf4j
+public class JwtUtil {
+
+  private final SecretKey secretKey;
+
+  /**
+   * JwtUtil constructor.
+
+   * @param secret private_key.
+   */
+  public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+
+    secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+        Jwts.SIG.HS256.key().build().getAlgorithm());
+  }
+
+  /**
+   * get email from token.
+   *
+   * @param token access, refresh token.
+   * @return email.
+   */
+  public String getEmail(String token) {
+
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token).getPayload().get("email", String.class);
+  }
+
+  /**
+   * get role from token.
+
+   * @param token access, refresh token.
+   * @return role.
+   */
+  public String getRole(String token) {
+
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token).getPayload().get("role", String.class);
+  }
+
+  /**
+   * token validation.
+
+   * @param token access, refresh token.
+   * @return if valid return true;
+   */
+  public Boolean isValid(String token) {
+    try {
+      Jwts.parser().verifyWith(secretKey).build()
+          .parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    } catch (Exception e) {
+
+      log.error("token is invalid: {}", e.getMessage());
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * get type from token.
+
+   * @param token access, refresh token.
+   * @return type.
+   */
+  public String getType(String token) {
+
+    return Jwts.parser().verifyWith(secretKey).build()
+        .parseSignedClaims(token).getPayload().get("type", String.class);
+  }
+
+  /**
+   * create jwt token.
+
+   * @param email .
+   * @param role .
+   * @param expiredMs .
+   * @return jwt access, refresh token.
+   */
+  public String createJwt(String type, String email, String role, Long expiredMs) {
+
+    return Jwts.builder()
+        .claim("type", type)
+        .claim("email", email)
+        .claim("role", role)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + expiredMs))
+        .signWith(secretKey)
+        .compact();
+  }
+}
--- src/main/java/com/elice/bookstore/config/security/filter/LoginFilter.java
+++ src/main/java/com/elice/bookstore/config/security/filter/LoginFilter.java
@@ -1,15 +1,25 @@
 package com.elice.bookstore.config.security.filter;
 
+import com.elice.bookstore.config.security.authentication.jwt.JwtUtil;
+import com.elice.bookstore.config.security.authentication.refresh.domain.Refresh;
+import com.elice.bookstore.config.security.authentication.refresh.dto.ResponseCreateTokens;
+import com.elice.bookstore.config.security.authentication.refresh.repository.RefreshRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -20,8 +30,22 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
   private final AuthenticationManager authenticationManager;
 
-  public LoginFilter(AuthenticationManager authenticationManager) {
+  private final JwtUtil jwtUtil;
+
+  private final RefreshRepository refreshRepository;
+
+  /**
+   * login filter.
+
+   * @param authenticationManager .
+   * @param jwtUtil .
+   * @param refreshRepository .
+   */
+  public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil,
+                     RefreshRepository refreshRepository) {
     this.authenticationManager = authenticationManager;
+    this.jwtUtil = jwtUtil;
+    this.refreshRepository = refreshRepository;
   }
 
   @Override
@@ -30,6 +54,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
       HttpServletResponse response
   ) throws AuthenticationException {
 
+    System.out.println("LoginFilter.attemptAuthentication");
     String email = request.getParameter("email");
     String password = obtainPassword(request);
 
@@ -48,7 +73,38 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
       Authentication authResult
   ) throws IOException, ServletException {
 
-    super.successfulAuthentication(request, response, chain, authResult);
+    String email = authResult.getName();
+
+    Collection<? extends GrantedAuthority> authorities = authResult.getAuthorities();
+    Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+    GrantedAuthority auth = iterator.next();
+    String role = auth.getAuthority();
+
+    ResponseCreateTokens tokens = createTokens(email, role);
+
+    response.setHeader("access", tokens.accessToken());
+    response.addCookie(createCookie(tokens.refreshToken()));
+    response.setStatus(HttpStatus.OK.value());
+  }
+
+  private ResponseCreateTokens createTokens(String email, String role) {
+    String accessToken = jwtUtil.createJwt("access", email, role, 60 * 10 * 1000L);
+
+    String refreshToken = jwtUtil.createJwt("refresh", email, role, 60 * 60 * 24 * 1000L);
+
+    Date date = new Date(System.currentTimeMillis() + (60 * 60 * 24 * 1000L));
+    Refresh refresh = new Refresh(email, refreshToken, date.toString());
+    refreshRepository.save(refresh);
+
+    return new ResponseCreateTokens(accessToken, refreshToken);
+  }
+
+  private Cookie createCookie(String value) {
+    Cookie cookie = new Cookie("refresh", value);
+    cookie.setMaxAge(60 * 60 * 24);
+    cookie.setHttpOnly(true);
+
+    return cookie;
   }
 
   @Override
@@ -58,6 +114,6 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
       AuthenticationException failed
   ) throws IOException, ServletException {
 
-    super.unsuccessfulAuthentication(request, response, failed);
+    response.setStatus(401);
   }
 }
