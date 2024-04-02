ad7f2d377347adfeae00da54c1595d61a1b1176a
ji-jjang <jinyhehe@gmail.com>
2024-04-03 03:40:21 +0900

    feat: Security 전역 설정 추가, 컨트롤러 cors 설정 추가

--- src/main/java/com/elice/bookstore/config/security/SecurityConfig.java
+++ src/main/java/com/elice/bookstore/config/security/SecurityConfig.java
@@ -1,6 +1,11 @@
 package com.elice.bookstore.config.security;
 
+import com.elice.bookstore.config.security.authentication.jwt.JwtUtil;
+import com.elice.bookstore.config.security.authentication.refresh.repository.RefreshRepository;
+import com.elice.bookstore.config.security.filter.CustomLogoutFilter;
+import com.elice.bookstore.config.security.filter.JwtFilter;
 import com.elice.bookstore.config.security.filter.LoginFilter;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +16,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * Global Security Config.
@@ -20,8 +27,22 @@ public class SecurityConfig {
 
   private final AuthenticationConfiguration authenticationConfiguration;
 
-  public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+  private final JwtUtil jwtUtil;
+
+  private final RefreshRepository refreshRepository;
+
+  /**
+   * Security Config Dependency.
+   *
+   * @param authenticationConfiguration .
+   * @param jwtUtil                     .
+   * @param refreshRepository           .
+   */
+  public SecurityConfig(AuthenticationConfiguration authenticationConfiguration,
+                        JwtUtil jwtUtil, RefreshRepository refreshRepository) {
     this.authenticationConfiguration = authenticationConfiguration;
+    this.jwtUtil = jwtUtil;
+    this.refreshRepository = refreshRepository;
   }
 
   /**
@@ -35,20 +56,43 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http
+        .cors((cors) -> cors
+            .configurationSource(request -> {
+
+              CorsConfiguration config = new CorsConfiguration();
+
+              config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+              config.setAllowedMethods(Collections.singletonList("*"));
+              config.setAllowCredentials(true);
+              config.setAllowedHeaders(Collections.singletonList("*"));
+              config.setMaxAge(60 * 60L);
+              config.setExposedHeaders(Collections.singletonList("Authorization"));
+
+              return config;
+            }));
+
+    http
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable);
 
     http
         .authorizeHttpRequests((auth) -> auth
-            .requestMatchers("/api/v1/login", "/", "/api/v1/signup").permitAll()
-            .requestMatchers("/api/v1/admin").hasRole("ADMIN")
+            .requestMatchers("/v1/login", "/", "/v1/signup", "/v1/reissue").permitAll()
+            .requestMatchers("/v1/admin").hasRole("ADMIN")
             .anyRequest().authenticated());
 
     http
+        .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+    http
         .addFilterAt(
-            new LoginFilter(authenticationManager(authenticationConfiguration)),
+            new LoginFilter(authenticationManager(authenticationConfiguration),
+                jwtUtil, refreshRepository),
             UsernamePasswordAuthenticationFilter.class);
+
+    http
+        .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
 
     http
         .sessionManagement((session) -> session
