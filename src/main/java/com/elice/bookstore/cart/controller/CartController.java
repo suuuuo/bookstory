3c6b5b987ce1766b4e9dd5be056e63c7ee63f652
soo <yoons1609@gmail.com>
2024-04-05 01:34:15 +0900

    feat: responseEntity, requestBody, @requestparam -> requestbody; dto.get

--- src/main/java/com/elice/bookstore/cart/controller/CartController.java
+++ src/main/java/com/elice/bookstore/cart/controller/CartController.java
@@ -2,19 +2,20 @@ package com.elice.bookstore.cart.controller;
 
 import com.elice.bookstore.book.domain.Book;
 import com.elice.bookstore.book.domain.BookService;
-import com.elice.bookstore.cart.application.CartService;
+import com.elice.bookstore.cartbook.application.CartBookService;
 import com.elice.bookstore.cartbook.domain.CartBook;
 import com.elice.bookstore.cartbook.dto.CartBookDto;
-import com.elice.bookstore.cartbook.mapper.CartBookMapper;
-import com.elice.bookstore.cartbook.application.CartBookService;
 import com.elice.bookstore.user.domain.UserService;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,68 +27,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cart")
 public class CartController {
 
-  private final CartService cartService;
-  private final CartBookService cartBookService;
-  private final BookService bookService;
-  private final CartBookMapper cartBookMapper;
-
-  public CartController(CartService cartService, CartBookService cartBookService,
-      BookService bookService, CartBookMapper cartBookMapper) {
-    this.cartService = cartService;
-    this.cartBookService = cartBookService;
-    this.bookService = bookService;
-    this.cartBookMapper = cartBookMapper;
-  }
+  @Autowired private CartBookService cartBookService;
+  @Autowired private BookService bookService;
 
   /**
-   * 장바구니 화면 : 담긴 상품들 조회
+   * 장바구니 화면 : 담긴 상품들 조회 @RequestHeader("Authorization") String authorizationHeader user email로 조회?
+   * ;loadUserByUsername
    */
-  @GetMapping("")
-  public List<CartBookDto> CartPageTest(@RequestParam long userId) {
+  @GetMapping("/{user_id}")
+  public ResponseEntity<List<CartBookDto>> CartPageTest(
+      @PathVariable(value = "user_id") long userId) {
     List<CartBook> cartBooks = cartBookService.findAllCartBook(userId);
-    List<CartBookDto> cartBookDtos = cartBooks.stream()
-        .map(CartBookMapper::ToDto)
-        .collect(Collectors.toList());
+
+    List<CartBookDto> cartBookDtos = new ArrayList<>();
+    for (CartBook cartBook : cartBooks) {
+      cartBookDtos.add(new CartBookDto(cartBook));
+    }
 
     int totalPrice = 0;
     for (CartBook cartBook : cartBooks) {
       totalPrice += cartBook.getCount() * cartBook.getBook().getPrice(); // 총 가격
     }
     int cartCount = cartBooks.size(); // 장바구니 담겨있는 상품 종류 수
-    return cartBookDtos;
+    return ResponseEntity.ok(cartBookDtos);
   }
 
-
-  /**
-   * 장바구니에 책 담기 ; 이미 있는 상품은 count만 증가
-   */
-  @PostMapping("/{book_id}")
-  public CartBookDto CartAddTest(
-      @RequestParam long userId, @PathVariable(value = "book_id") long bookId) {
-    int count = 1; // 웹에서 가져와야?
-    Book book = bookService.findBook(bookId); // 책 아이디로 책 받아옴
-    return CartBookMapper.ToDto(cartBookService.addCartBook(userId, book, count));
+  /** 장바구니에 책 담기 ; 장바구니 담기 버튼;처음 담는 상품 추가, 이미 있는 상품은 count만 증가 */
+  @PostMapping("/{user_id}/add")
+  public ResponseEntity<CartBookDto> CartAddTest(
+      @PathVariable(value = "user_id") long userId, @RequestBody CartBookDto cartBookDto) {
+    Book book = bookService.findBook(cartBookDto.getBookId()); // 책 아이디로 책 받아옴
+    CartBookDto c =
+        new CartBookDto(cartBookService.addCartBook(userId, book, cartBookDto.getCount()));
+    return ResponseEntity.ok(c);
   }
 
   /**
-   * 웹에서 조작한 상품 개수 반영
+   * 장바구니에 책 담기 ; 웹에서 조작한 상품 개수 반영 * / @PutMapping("/patch") public ResponseEntity<CartBookDto>
+   * CartPatch( // @PathVariable(value = "cartBook_id") long cartBookId, @RequestBody CartBookDto
+   * cartBookDto) { CartBookDto c = new
+   * CartBookDto(cartBookService.patchCartBook(cartBookDto.getId(), cartBookDto.getCount())); return
+   * ResponseEntity.ok(c); }
+   *
+   * <p>/**장바구니에서 상품 삭제
    */
-  @PutMapping("/{cartBook_id}")
-  public CartBookDto CartPatch(
-      @PathVariable(value = "cartBook_id") long cartBookId, @RequestParam int count){
-    CartBook cartBook = cartBookService.patchCartBook(cartBookId, count);
-    return cartBookMapper.ToDto(cartBook);
-  }
-
-
-  /**
-   * 장바구니에서 상품 삭제
-   */
-  @DeleteMapping("{cartbook_id}")
-  public void DeleteCartBook(
-      @PathVariable(value = "cartBook_id") long cartBookId) {
-
-    // 실제 동작시에는 웹에서 선택된 것들만 리스트로 받는 : 책 id로 받?
-    cartBookService.deleteCartBook(cartBookId);
+  @DeleteMapping("/delete")
+  public void DeleteCartBook(@RequestBody CartBookDto cartBookDto) {
+    // 프론트엔드에서 체크박스 상태 확인해서 체크된 것들만 요청
+    cartBookService.deleteCartBook(cartBookDto.getId());
   }
 }
