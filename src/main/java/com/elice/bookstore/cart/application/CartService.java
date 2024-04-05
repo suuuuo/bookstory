0a35d82884e7f3615fa571214b4f50c8d0f798fa
soo <yoons1609@gmail.com>
2024-04-06 01:44:46 +0900

    feat: cartbookservice 처리 후 dto 반환

--- src/main/java/com/elice/bookstore/cart/application/CartService.java
+++ src/main/java/com/elice/bookstore/cart/application/CartService.java
@@ -13,19 +13,18 @@ public class CartService {
   @Autowired
   private CartRepository cartRepository;
 
-  @Autowired
-  private UserRepository userRepository;
+  // 토큰으로 유저 정보 가져오는 걸로 수정 해야
+  // @Autowired
+  // private UserRepository userRepository;
 
-  /**
-   * 장바구니 조회
-   */
+  /** 장바구니 조회 */
   @Transactional
-  public Cart findCart(long userId){ //유저 정보로 장바구니 가져오기
+  public Cart findCart(long userId) { // 유저 정보로 장바구니 가져오기
     Cart cart = cartRepository.findByUserId(userId);
 
     if(cart == null){ // 유저에게 장바구니가 없으면 유저 정보 가져와서 장바구니 생성
-      User user = userRepository.findById(userId); // user는 cart정보 가지고 있지 않으므로 조회
-      cart = new Cart(user);
+      //   User user = userRepository.findById(userId); // user는 cart정보 가지고 있지 않으므로 조회
+      //   cart = new Cart(user);
       cartRepository.save(cart);
     }
     return cart;
