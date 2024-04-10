1d2247511bc7ba1924ed05115cc053edb4f5f74c
ji-jjang <jinyhehe@gmail.com>
2024-04-10 14:45:01 +0900

    fix: 매핑되지 않은 필드 경고문 제거

--- src/main/java/com/elice/bookstore/book/domain/mapper/BookMapper.java
+++ src/main/java/com/elice/bookstore/book/domain/mapper/BookMapper.java
@@ -4,9 +4,10 @@ import com.elice.bookstore.book.domain.Book;
 import com.elice.bookstore.book.domain.dto.RequestBook;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BookMapper {
 
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
