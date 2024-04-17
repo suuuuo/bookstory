543a5d37195fc843dc01f30adad9bd0671da4321
jinyus <jinyhehe@gmail.com>
2024-04-17 14:36:43 +0000

    feat: security cors 서버 설정으로 변경(nginx)

--- src/main/java/com/elice/bookstore/config/security/SecurityConfig.java
+++ src/main/java/com/elice/bookstore/config/security/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
 
               CorsConfiguration config = new CorsConfiguration();
 
-              config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+              config.setAllowedOrigins(Collections.singletonList("http://34.22.73.96"));
               config.setAllowedMethods(Collections.singletonList("*"));
               config.setAllowCredentials(true);
               config.setAllowedHeaders(Collections.singletonList("*"));
